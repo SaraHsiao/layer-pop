18f50d8f402d4741b3a6746d40028e09c240365e
Sara <kafeidou0909@gmail.com>
2017-11-05 15:13:27 +0800

    Complete

---  js/index.js
+++  js/index.js
@@ -1,16 +1,63 @@
 
+$(document).ready(function($) {
 
-$(document).ready(function() {
-
-    // 登入連結事件
+    // login
     $('#loginLink').click(function() {
+        var loginHtml=$('#loginHtml').html();
+        showLayer(loginHtml,500,300,closeCallback);
+
+        $('#loginSubmitBtn').click(function() {
+            var username=$("input[name='username']").val();
+            var password=$("input[name='password']").val();
+
+            if(username === '1234' && password === '1234') {
+                alert('登入成功');
+            } else {
+                $('.error-msg').html("帳號或密碼輸入錯誤");
+            }
+        });
+    });
+
+    $('#regeLink').click(function() {
+        var regeHtml=$('#regeHtml').html();
+        showLayer(regeHtml,500,350,closeCallback);
+
+        $('#regeSubmitBtn').click(function() {
+            var username=$("input[name='username']").val();
+            var password=$("input[name='password']").val();
+            var repassword=$("input[name='repassword']").val();
+
+            if(username === '1234' && password === '1234' && repassword === password) {
+                alert('註冊成功');
+            } else {
+                $('.error-msg').html('帳號或是密碼輸入錯誤')
+            }
+        })
+    })
+
+    function closeCallback(){
+        $('.error-msg').html("");
+    }
+
+    function showLayer(html,width,height,closeCallback){
         $('#layer-mask').show();
         $('#layer-pop').show();
 
-        // 關閉連結事件
+        $("#layer-pop").css({
+            width: width,
+            height: height
+        });
+
+        $('#layer-content').html(html);
+
         $('#layer-close').click(function(){
-            $('#layer-mask').hide();
-            $('#layer-pop').hide();
+            hideLayer();
+            closeCallback();
         })
-    })
-})
+    }
+
+    function hideLayer() {
+        $('#layer-mask').hide();
+        $('#layer-pop').hide();
+    }
+});
